18c4ce8aadf7db351e9dde32303019388dfc9e9a
sebastian beltran <sebastian199502@hotmail.com>
2021-11-19 14:22:44 -0500

    PHP-Ejercicio-2-v2

---
+++ analisis.php
@@ -0,0 +1,17 @@
+<?php
+// print_r($_REQUEST);
+
+$palabras = array("sol", "luna", "cielo", "luz", "estrellas", "lluvia");
+
+
+for($i = 0; $i < count($palabras); $i++){
+    if($_REQUEST["palabra" .$i] == $palabras[$i]){
+        echo "La palabra ingresada es correcta" ."<br>";
+    } else {
+        echo "La palabra ingresada es incorrecta, la palabra correcta es: " .$palabras[$i] ."<br>";
+    }
+}
+
+
+
+ 
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$palabras = array("sol", "luna", "cielo", "luz", "estrellas", "lluvia");
+
+
+$form ="<form action='analisis.php'>";
+
+for($i = 0; $i < count($palabras); $i++){
+    $form .="La palabra: " .str_shuffle($palabras[$i]). " ".
+    "<input type='text' name='palabra".$i."'>".
+    "<br>";
+}
+
+$button = "<button type='submit'>Enviar</button>";
+$formCierre = "</form>";
+
+
+echo $form.$button.$formCierre;
+
+
+
+
